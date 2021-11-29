1aaaee23d5235c85ec417215611c219f9fa7fa4b
Hayato Kihara <kai.21banana@gmail.com>
2021-11-29 22:56:12 +0900

    update .circleci/config.yml

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,11 +1,11 @@
 import kotlinx.coroutines.delay
 import kotlinx.coroutines.launch
 import kotlinx.coroutines.runBlocking
-import kotlinx.serialization.json.Json // Unused import directive
+//import kotlinx.serialization.json.Json // Unused import directive
 
 fun main() = runBlocking {
     launch {
-        Thread.sleep(10000) // Inappropriate blocking method call
+//        Thread.sleep(10000) // Inappropriate blocking method call
         println("second")
     }
 
